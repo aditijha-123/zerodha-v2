5693696177ee5cd32e72461cb91035398cc1fb94
Aditi Jha <you@example.com>
2025-02-21 15:37:49 +0530

    href error solved

--- src/Footer.js
+++ src/Footer.js
@@ -1,66 +1,66 @@
-import React from 'react';
-import "./index.css"; // Assuming you create a CSS file for styling
+import React from 'react'
 
 function Footer() {
   return (
-    <div className='bg-light'>
-      <div className="container p-5">
-        <div className="row">
-          <div className="col">
-            <img src='images/logo.svg' alt='logo' style={{ width: "50%" }} />
-            <p>
-              © 2010 - 2024, Zerodha Broking Ltd. <br />
-              All rights reserved.
-            </p>
-            <div className="social-icons">
-              <span className='fake-link'><i className="fa-brands fa-x-twitter"></i></span>
-              <span className='fake-link'><i className="fa-brands fa-square-facebook"></i></span>
-              <span className='fake-link'><i className="fa-brands fa-instagram"></i></span>
-              <span className='fake-link'><i className="fa-brands fa-linkedin-in"></i></span>
-              <span className='fake-link'><i className="fa-brands fa-youtube"></i></span>
-              <span className='fake-link'><i className="fa-brands fa-whatsapp"></i></span>
-              <span className='fake-link'><i className="fa-brands fa-telegram"></i></span>
-            </div>
-          </div>
-
-          <div className="col">
-            <h5>Company</h5>
-            <span className='fake-link'>About</span><br />
-            <span className='fake-link'>Products</span><br />
-            <span className='fake-link'>Pricing</span><br />
-            <span className='fake-link'>Referral programme</span><br />
-            <span className='fake-link'>Careers</span><br />
-            <span className='fake-link'>Zerodha.tech</span><br />
-            <span className='fake-link'>Press & media</span><br />
-            <span className='fake-link'>Zerodha Cares (CSR)</span>
-          </div>
-
-          <div className="col">
-            <h5>Support</h5>
-            <span className='fake-link'>Contact us</span><br />
-            <span className='fake-link'>Support portal</span><br />
-            <span className='fake-link'>Z-Connect blog</span><br />
-            <span className='fake-link'>List of charges</span><br />
-            <span className='fake-link'>Downloads & resources</span><br />
-            <span className='fake-link'>Videos</span><br />
-            <span className='fake-link'>How to file a complaint?</span><br />
-            <span className='fake-link'>Status of your complaints</span><br />
-          </div>
-
-          <div className="col">
-            <h5>Account</h5>
-            <span className='fake-link'>Open an account</span><br />
-            <span className='fake-link'>Fund transfer</span><br />
-          </div>
-        </div>
-
-        <div className='mt-5'>
-          <p>Investments in securities market are subject to market risks; read all the related documents carefully before investing.</p>
-          <p>Attention investors: Stock brokers can accept securities as margins from clients only by way of pledge in the depository system w.e.f September 01, 2020.</p>
-        </div>
-      </div>
-    </div>
-  );
+   <div className='bg-light'>
+   <div className="container p-5">
+   <div className="row ">
+   <div className="col">
+   <img src='images/logo.svg' alt='logo' style={{width : "50%"}}/>
+   <p>© 2010 - 2024, Zerodha Broking Ltd. <br/>
+   All rights reserved.</p>
+   <div>
+   <a href='#' style={{marginRight:"20px",color:"#000",fontSize:"20px"}} className='text-muted' ><i class="fa-brands fa-x-twitter"></i></a>
+   <a href='#' style={{marginRight:"20px",color:"#000",fontSize:"20px"}}className='text-muted'><i class="fa-brands fa-square-facebook"></i></a>
+   <a href='#' style={{marginRight:"20px",color:"#000",fontSize:"20px"}}className='text-muted'><i class="fa-brands fa-instagram"></i></a>
+   <a href='#' style={{marginRight:"20px",color:"#000",fontSize:"20px"}} className='text-muted'><i class="fa-brands fa-linkedin-in"></i></a>
+   </div>
+   <div>
+   <br/>
+   <a href='#' style={{marginRight:"20px",color:"#000",fontSize:"20px"}} className='text-muted'><i class="fa-brands fa-youtube"></i></a>
+   <a href='#' style={{marginRight:"30px",color:"#000",fontSize:"20px"}} className='text-muted'><i class="fa-brands fa-whatsapp"></i></a>
+   <a href='#' style={{marginRight:"20px",color:"#000",fontSize:"20px"}} className='text-muted'><i class="fa-brands fa-telegram"></i></a>
+   </div>
+   </div>
+   <div className="col">
+   Company<br/>
+   <a href="/">About</a><br/>
+   <a href="/">Products</a><br/>
+   <a href="/">Pricing</a><br/>
+   <a href="/">Referral programme</a><br/>
+   <a href="/">Careers</a><br/>
+   <a href="/">Zerodha.tech</a><br/>
+   <a href="/">Press & media</a><br/>
+   <a href="/">Zerodha Cares (CSR)</a>
+   </div>
+   <div className="col">
+   Support<br/>
+   <a href="/"> Contact us </a><br/>
+   <a href="/"> Support portal</a><br/>
+   <a href="/"> Z-Connect blog</a><br/>
+   <a href="/"> List of charges</a><br/>
+   <a href="/"> Downloads & resources</a><br/>
+   <a href="/"> Videos</a><br/>
+   <a href="/"> How to file a complaint?</a><br/>
+   <a href="/"> Status of your complaints</a><br/>
+   </div>
+   <div className="col">
+   Account<br/>
+   <a href="/">Open an account</a><br/>
+   <a href="/">Fund transfer</a><br/>
+   </div>
+   </div>
+    <div className='mt-5'>
+   <p>Zerodha Broking Ltd.: Member of NSE, BSE​ &​ MCX – SEBI Registration no.: INZ000031633 CDSL/NSDL: Depository services through Zerodha Broking Ltd. – SEBI Registration no.: IN-DP-431-2019 Commodity Trading through Zerodha Commodities Pvt. Ltd. MCX: 46025; NSE-50001 – SEBI Registration no.: INZ000038238 Registered Address: Zerodha Broking Ltd., #153/154, 4th Cross, Dollars Colony, Opp. Clarence Public School, J.P Nagar 4th Phase, Bengaluru - 560078, Karnataka, India. For any complaints pertaining to securities broking please write to complaints@zerodha.com, for DP related to dp@zerodha.com. Please ensure you carefully read the Risk Disclosure Document as prescribed by SEBI | ICF</p>
+   <p>Procedure to file a complaint on SEBI SCORES: Register on SCORES portal. Mandatory details for filing complaints on SCORES: Name, PAN, Address, Mobile Number, E-mail ID. Benefits: Effective Communication, Speedy redressal of the grievances</p>
+   <p>Smart Online Dispute Resolution | Grievances Redressal Mechanism</p>
+   <p>Investments in securities market are subject to market risks; read all the related documents carefully before investing.</p>
+   <p>Attention investors: 1) Stock brokers can accept securities as margins from clients only by way of pledge in the depository system w.e.f September 01, 2020. 2) Update your e-mail and phone number with your stock broker / depository participant and receive OTP directly from depository on your e-mail and/or mobile number to create pledge. 3) Check your securities / MF / bonds in the consolidated account statement issued by NSDL/CDSL every month.</p>
+   <p>"Prevent unauthorised transactions in your account. Update your mobile numbers/email IDs with your stock brokers. Receive information of your transactions directly from Exchange on your mobile/email at the end of the day. Issued in the interest of investors. KYC is one time exercise while dealing in securities markets - once KYC is done through a SEBI registered intermediary (broker, DP, Mutual Fund etc.), you need not undergo the same process again when you approach another intermediary." Dear Investor, if you are subscribing to an IPO, there is no need to issue a cheque. Please write the Bank account number and sign the IPO application form to authorize your bank to make payment in case of allotment. In case of non allotment the funds will remain in your bank account. As a business we don't give stock tips, and have not authorized anyone to trade on behalf of others. If you find anyone claiming to be part of Zerodha and offering such services, please create a ticket here.</p>
+   </div>
+   </div>
+   </div>
+  )
 }
 
 export default Footer;
